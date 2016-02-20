421e5da05efd819bab206726ed6047d5ad7bbc38
Joe Kutner <jpkutner@gmail.com>
2016-02-20 10:18:23 -0600

    renamed a variable

--- app.rb
+++ app.rb
@@ -3,8 +3,8 @@ require 'jbundler'
 
 java_import 'ratpack.server.RatpackServer'
 
-RatpackServer.start do |server|
-  server.handlers do |chain|
+RatpackServer.start do |b|
+  b.handlers do |chain|
     chain.get("") do |ctx|
       ctx.render("Hello from Ratpack JRuby")
     end
