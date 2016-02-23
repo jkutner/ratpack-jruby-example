330774dd6bbfc595df6164ff349b88e8601fdc2f
Joe Kutner <jpkutner@gmail.com>
2016-02-23 14:16:24 -0600

    Improve examples for registry and stream

--- app.rb
+++ app.rb
@@ -1,14 +1,14 @@
 require 'java'
+require 'jruby/core_ext'
 require 'bundler/setup'
-require 'jbundler'
-require 'sequel'
-require 'json'
-require 'jdbc-url'
+Bundler.require
 
 java_import 'ratpack.server.RatpackServer'
 java_import 'ratpack.registry.Registry'
-
-JavaString = java.lang.Class.forName("java.lang.String")
+java_import 'ratpack.exec.Blocking'
+java_import 'ratpack.stream.Streams'
+java_import 'ratpack.http.ResponseChunks'
+java_import 'java.time.Duration'
 
 DB = Sequel.connect(JdbcUrl.from_database_url)
 
@@ -16,18 +16,32 @@ DB.run "CREATE TABLE IF NOT EXISTS widgets (id serial not null primary key, name
 
 class Widget < Sequel::Model
   plugin :json_serializer
+  @java_class = become_java!
+  def self.java_class
+    @java_class
+  end
 end
 
 RatpackServer.start do |b|
   b.handlers do |chain|
-    chain.get("") do |ctx|
+    chain.get do |ctx|
       ctx.render("Hello from Ratpack JRuby")
     end
 
+    chain.get("stream") do |ctx|
+      publisher = Streams.periodically(ctx, Duration.ofMillis(5)) do |i|
+        i < 10 ? i.to_s : nil
+      end
+      ctx.render(ResponseChunks.stringChunks(publisher))
+    end
+
     chain.prefix("widgets") do |c1|
-      c1.get("") do |ctx|
-        count = DB[:widgets].count
-        ctx.render("all of them: #{count.inspect}")
+      c1.get do |ctx|
+        Blocking.get do
+          DB[:widgets].count
+        end.then do |i|
+          ctx.render(JSON.dump({count: i}))
+        end
       end
 
       c1.post("new") do |ctx|
@@ -40,13 +54,18 @@ RatpackServer.start do |b|
       c1.prefix(":id") do |c2|
         c2.all do |ctx|
           id = ctx.path_tokens['id']
-          ctx.next(Registry.single(id))
+          widget = Widget[id.to_i]
+          ctx.next(Registry.single(Widget.java_class, widget))
         end
 
         c2.get do |ctx|
-          id = ctx.get(JavaString)
-          widget = Widget[id.to_i]
+          widget = ctx.get(Widget.java_class)
           ctx.render(widget.to_json)
+        end
+
+        c2.get("name") do |ctx|
+          widget = ctx.get(Widget.java_class)
+          ctx.render(widget.name)
         end
       end
     end
